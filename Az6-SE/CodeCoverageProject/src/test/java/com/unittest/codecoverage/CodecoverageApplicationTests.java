828099878404c1a718cfc492e3cd1566dbbcf1a1
Nima Salem <nima@Nimas-MacBook-Air.local>
2022-12-30 06:58:54 +0330

    models coverage

--- Az6-SE/CodeCoverageProject/src/test/java/com/unittest/codecoverage/CodecoverageApplicationTests.java
+++ Az6-SE/CodeCoverageProject/src/test/java/com/unittest/codecoverage/CodecoverageApplicationTests.java
@@ -2,6 +2,8 @@ package com.unittest.codecoverage;
 
 import com.unittest.codecoverage.exceptions.BehaviorException;
 import com.unittest.codecoverage.exceptions.PersonException;
+import com.unittest.codecoverage.models.*;
+import com.unittest.codecoverage.models.validators.PersonValidator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,6 +29,31 @@ class CodecoverageApplicationTests {
 		} catch (PersonException e){
 			assertTrue(true);
 		}
+	}
+	@Test
+	void testModelPackage() {
+		Gender female_g = Gender.F;
+		Person p = new Person();
+		p.setGender(female_g);
+		p.setName("new Female");
+		PersonValidator pv = new PersonValidator();
+		boolean result = pv.requiredName(p);
+
+		assertEquals("new Female", p.getName());
+		assertTrue(result);
+
+		TrafficLigth green_tr = TrafficLigth.GREEN;
+		Footpassenger fp = new Footpassenger();
+		fp.setCrossedTrafficLigth(green_tr);
+
+		assertEquals(green_tr, fp.getCrossedTrafficLigth());
+
+		StreetDirectionFlow one_way_str = StreetDirectionFlow.ONE_WAY;
+		Traffic traffic = new Traffic();
+		traffic.setStreetDirectionFlow(one_way_str);
+
+		assertEquals(one_way_str, traffic.getStreetDirectionFlow());
+
 
 	}
 
